4187932064ebb670188a39ed2fa91a0d3bf9e146
rihilke <perevetish@yandex.ru>
2022-04-03 22:30:49 +0300

    start

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+
+        Server server = new Server(8080);
+        server.setHandler(handlers);
+
+        server.start();
+        System.out.println("Server started");
+
+        server.join();
+
+    }
+}
